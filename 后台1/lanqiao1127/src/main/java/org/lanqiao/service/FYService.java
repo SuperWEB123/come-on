2b1a8a0cd4e7d830ebb86cdb993be7064b6829d6
Jackeq <58057328+Jackeq@users.noreply.github.com>
2019-12-16 10:04:26 +0800

    后台页面

---
+++ 后台1/lanqiao1127/src/main/java/org/lanqiao/service/FYService.java
@@ -0,0 +1,20 @@
+package org.lanqiao.service;
+
+import org.lanqiao.dao.FYdao;
+
+import java.sql.SQLException;
+
+public class FYService {
+    FYdao dao = new FYdao();
+
+    public int add(double hmoney, double harea, String haddress, String housetype, String hsynopsis,String substring) throws SQLException {
+        return dao.fy(hmoney,harea,  haddress,  housetype, hsynopsis,substring);
+    }
+
+//    public int add(Fang f) throws SQLException {
+//
+//    }
+
+//    public int add(double hmoney, double harea, String haddress, String housetype, String hsynopsis) {
+//    }
+}
